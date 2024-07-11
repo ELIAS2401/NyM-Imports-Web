f85a7b168ea0ac44d3491ad0d98bbd4ee80793a6
ELIAS2401 <tuccieliasluis@gmail.com>
2024-07-11 16:32:06 -0300

    11/7 Responsive del nav
    Quitar opcion niños
    Responsive del nav. En el celular no se mostrarán opciones, al tocar "Hombres" te llevara a una pagina secundaria con secciones de pantalones remeras camperas etc. y asi con cada enlace.
    En ipad se abrira el menu de opciones al hacer click. En pc al pasar el mouse (mouseover)

--- scripts/script.js
+++ scripts/script.js
@@ -17,13 +17,68 @@ function ocultar(articulo) {
     })
 }
 
-hombres.addEventListener("mouseover", () => mostrar(articuloHombres))
-hombres.addEventListener("mouseout", () => ocultar(articuloHombres))
-
 //mujeres
 
 let mujeres = document.getElementById("mujerEnlace");
 let articuloMujeres = document.getElementById("articuloMujer");
 
-mujeres.addEventListener("mouseover", () => mostrar(articuloMujeres))
-mujeres.addEventListener("mouseout", () => ocultar(articuloMujeres))
+//accesorios
+
+let accesorios = document.getElementById("accesoriosEnlace");
+let articuloAccesorios = document.getElementById("articuloAccesorios");
+
+//Responsive ipad
+
+function toggleIpad(elemento) {
+    elemento.classList.toggle("ocultar");
+}
+
+function togglear(e, articuloAMostrar) {
+    e.preventDefault();
+    toggleIpad(articuloAMostrar);
+}
+function hombresClick(e) {
+    togglear(e, articuloHombres);
+}
+
+function mujeresClick(e) {
+    togglear(e, articuloMujeres);
+}
+
+function accesoriosClick(e) {
+    togglear(e, articuloAccesorios);
+}
+//Responive
+
+function aplicarEventos() {
+    if (window.matchMedia("(min-width: 1024px)").matches) {
+        hombres.addEventListener("mouseover", () => mostrar(articuloHombres))
+        hombres.addEventListener("mouseout", () => ocultar(articuloHombres))
+        mujeres.addEventListener("mouseover", () => mostrar(articuloMujeres))
+        mujeres.addEventListener("mouseout", () => ocultar(articuloMujeres))
+        accesorios.addEventListener("mouseover", () => mostrar(articuloAccesorios))
+        accesorios.addEventListener("mouseout", () => ocultar(articuloAccesorios))
+    }
+    else if (window.matchMedia("(min-width: 768px)").matches) {
+        hombres.addEventListener("click", hombresClick);
+        mujeres.addEventListener("click", mujeresClick);
+        accesorios.addEventListener("click", accesoriosClick);
+    }
+    else {
+        hombres.removeEventListener("mouseover", () => mostrar(articuloHombres))
+        hombres.removeEventListener("mouseout", () => ocultar(articuloHombres))
+        mujeres.removeEventListener("mouseover", () => mostrar(articuloMujeres))
+        mujeres.removeEventListener("mouseout", () => ocultar(articuloMujeres))
+        accesorios.removeEventListener("mouseover", () => mostrar(articuloAccesorios))
+        accesorios.removeEventListener("mouseout", () => ocultar(articuloAccesorios))
+
+        hombres.removeEventListener("click", hombresClick);
+        mujeres.removeEventListener("click", mujeresClick);
+        accesorios.removeEventListener("click", accesoriosClick)
+    }
+}
+
+aplicarEventos();
+
+window.addEventListener('resize', applyListeners);
+
