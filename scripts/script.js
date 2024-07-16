079c7538cc2e220e5e2365adc946803ec79f8ee4
alexisvh5 <109443139+alexisvh5@users.noreply.github.com>
2024-07-16 14:11:12 -0300

    cambio tarjetas, faltan los div-check

--- scripts/script.js
+++ scripts/script.js
@@ -27,3 +27,40 @@ let articuloMujeres = document.getElementById("articuloMujer");
 
 mujeres.addEventListener("mouseover", () => mostrar(articuloMujeres))
 mujeres.addEventListener("mouseout", () => ocultar(articuloMujeres))
+
+
+
+
+/// haciendo el js de las tarjetas -----
+
+let elementoImagenZapatilla = document.getElementById('imagen-tarjeta-zapatilla');
+let elemento= document.querySelector('.imagen-tarjeta-zapatilla')
+
+let imagenOriginalZapatilla = '/assets/ropa/1.jpeg';
+let nuevaImagenZapatilla = '/assets/ropa/2.jpeg'; // Cambia esta URL a la nueva imagen
+
+elementoImagenZapatilla.addEventListener('mouseover', function() {
+    elemento.style.backgroundImage = `url(${nuevaImagenZapatilla})`;
+});
+
+elementoImagenZapatilla.addEventListener('mouseout', function() {
+    elemento.style.backgroundImage = `url(${imagenOriginalZapatilla})`;
+});
+
+
+
+/////
+
+
+let elementoImagenAccesorio = document.getElementById('imagen-tarjeta-accesorio');
+let elementoAccesorio= document.querySelector('.imagen-tarjeta-accesorio')
+let imagenOriginalAccesorio = '/assets/ropa/3.jpeg';
+let nuevaImagenAccesorio = '/assets/ropa/4.jpeg'; // Cambia esta URL a la nueva imagen
+
+elementoImagenAccesorio.addEventListener('mouseover', function() {
+    elementoAccesorio.style.backgroundImage = `url(${nuevaImagenAccesorio})`;
+});
+
+elementoImagenAccesorio.addEventListener('mouseout', function() {
+    elementoAccesorio.style.backgroundImage = `url(${imagenOriginalAccesorio})`;
+});
