ddf828327b5a2775a839972d2a397090aa99ca3d
alexisvh5 <109443139+alexisvh5@users.noreply.github.com>
2024-07-20 20:14:09 -0300

    mejoro transision entre imagenes, falta otros productos y ajustar tarjetas responsive

--- scripts/script.js
+++ scripts/script.js
@@ -35,32 +35,48 @@ mujeres.addEventListener("mouseout", () => ocultar(articuloMujeres))
 
 let elementoImagenZapatilla = document.getElementById('imagen-tarjeta-zapatilla');
 let elemento= document.querySelector('.imagen-tarjeta-zapatilla')
-
 let imagenOriginalZapatilla = '/assets/ropa/1.jpeg';
 let nuevaImagenZapatilla = '/assets/ropa/2.jpeg'; // Cambia esta URL a la nueva imagen
 
+
 elementoImagenZapatilla.addEventListener('mouseover', function() {
-    elemento.style.backgroundImage = `url(${nuevaImagenZapatilla})`;
+    elementoImagenZapatilla.style.opacity = '0';
+    setTimeout(() => {
+        elemento.style.opacity = '1';
+        elemento.style.backgroundImage = `url(${nuevaImagenZapatilla})`;
+       
+    }, 300); // Ajusta este valor al mismo que el de la transición
 });
 
 elementoImagenZapatilla.addEventListener('mouseout', function() {
-    elemento.style.backgroundImage = `url(${imagenOriginalZapatilla})`;
+    elementoImagenZapatilla.style.opacity = '0';
+    setTimeout(() => {
+        elemento.style.backgroundImage = `url(${imagenOriginalZapatilla})`;
+        elemento.style.opacity = '1';
+    }, 300); // Ajusta este valor al mismo que el de la transición
 });
-
 
 
 /////
 
-
 let elementoImagenAccesorio = document.getElementById('imagen-tarjeta-accesorio');
-let elementoAccesorio= document.querySelector('.imagen-tarjeta-accesorio')
+let elementoAccesorio = document.querySelector('.imagen-tarjeta-accesorio');
 let imagenOriginalAccesorio = '/assets/ropa/3.jpeg';
-let nuevaImagenAccesorio = '/assets/ropa/4.jpeg'; // Cambia esta URL a la nueva imagen
+let nuevaImagenAccesorio = '/assets/ropa/4.jpeg';
 
 elementoImagenAccesorio.addEventListener('mouseover', function() {
-    elementoAccesorio.style.backgroundImage = `url(${nuevaImagenAccesorio})`;
+    elementoAccesorio.style.opacity = '0';
+    setTimeout(() => {
+        elementoAccesorio.style.opacity = '1';
+        elementoAccesorio.style.backgroundImage = `url(${nuevaImagenAccesorio})`;
+       
+    }, 300); // Ajusta este valor al mismo que el de la transición
 });
 
 elementoImagenAccesorio.addEventListener('mouseout', function() {
-    elementoAccesorio.style.backgroundImage = `url(${imagenOriginalAccesorio})`;
+    elementoAccesorio.style.opacity = '0';
+    setTimeout(() => {
+        elementoAccesorio.style.backgroundImage = `url(${imagenOriginalAccesorio})`;
+        elementoAccesorio.style.opacity = '1';
+    }, 300); // Ajusta este valor al mismo que el de la transición
 });
